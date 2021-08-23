32d284c20032ef7e7b6cb77bd4c89dee685953a7
Huan Ai <aihuan96@gmail.com>
2021-08-23 19:29:50 -0400

    working now

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/register', function(req, res, next))
+
+
+
 
 module.exports = router;
