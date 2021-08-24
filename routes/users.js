246d5f23f3442f830ea61300d63fd43623a9a042
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2021-08-24 09:22:45 -0700

    Finished the get route for register and most of the post route for register.

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,60 @@ var router = express.Router();
 const { check, validationResult } = require('express-validator');
 
 const db = require('../db/models');
-const { csrfProtection, asyncHandler } = require('./utils');
+const {
+   bcrypt,
+   csrfProtection,
+    asyncHandler 
+  } = require('./utils');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/register', function(req, res, next))
+router.get('/register', csrfProtection, asyncHandler( async (req, res, next) => {
+  const user = await db.User.build();
+  res.render('user-register', {
+    title: 'Register with RetroGameTracker',
+    user,
+    csrfToken: req.csrfToken()
+  })
+}));
 
+router.post('/register', csrfProtection, asyncHandler( async (req, res, next) => {
+  const {
+    email,
+    username,
+    password
+  } = req.body;
 
+  const user = db.User.build({
+    email,
+    username,
+  })
 
+  //TODO: Need to call validators here and save it to a variable
+  //  Please change null to correct value when time comes
+  const validationErrors = null;
+
+  if (validationErrors.isEmpty()){
+    
+    user.password = await bcrypt.hash(password, 10);
+    await user.save();
+    res.redirect('/');
+  
+  } else {
+  
+    const errors = validationErrors.array().map(error => error.msg);
+    res.render('user-register', {
+      title: 'Register with RetroGameTracker',
+      user,
+      errors,
+      csrfToken: req.csrfToken()
+    });
+  
+  }
+
+}));
 
 module.exports = router;
--- routes/utils.js
+++ routes/utils.js
@@ -2,10 +2,13 @@ const csrf = require('csurf');
 
 const csrfProtection = csrf({ cookie: true });
 
+const bcrypt = require(`bcryptjs`);
+
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 
 module.exports = {
+    bcrypt,
     csrfProtection,
     asyncHandler,
 };
