da9add93b4f5ea2457c0638b228e4b79d987da83
Johnny Park <johnpark1987@gmail.com>
2021-08-24 10:15:44 -0700

    completed user validation checking

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 var express = require('express');
+const { validationResult } = require('express-validator');
 var router = express.Router();
-const { check, validationResult } = require('express-validator');
+
 
 const db = require('../db/models');
 const {
-   bcrypt,
-   csrfProtection,
-    asyncHandler 
+  bcrypt,
+  csrfProtection,
+  asyncHandler,
+  userValidators
   } = require('./utils');
 
 /* GET users listing. */
@@ -23,11 +25,11 @@ router.get('/register', csrfProtection, asyncHandler( async (req, res, next) => 
   })
 }));
 
-router.post('/register', csrfProtection, asyncHandler( async (req, res, next) => {
+router.post('/register', csrfProtection, userValidators, asyncHandler( async (req, res, next) => {
   const {
     email,
     username,
-    password
+    password,
   } = req.body;
 
   const user = db.User.build({
@@ -37,16 +39,16 @@ router.post('/register', csrfProtection, asyncHandler( async (req, res, next) =>
 
   //TODO: Need to call validators here and save it to a variable
   //  Please change null to correct value when time comes
-  const validationErrors = null;
+  const validationErrors = validationResult(req);
 
   if (validationErrors.isEmpty()){
-    
-    user.password = await bcrypt.hash(password, 10);
+
+    user.hashedPassword = await bcrypt.hash(password, 10);
     await user.save();
     res.redirect('/');
-  
+
   } else {
-  
+
     const errors = validationErrors.array().map(error => error.msg);
     res.render('user-register', {
       title: 'Register with RetroGameTracker',
@@ -54,9 +56,7 @@ router.post('/register', csrfProtection, asyncHandler( async (req, res, next) =>
       errors,
       csrfToken: req.csrfToken()
     });
-  
   }
-
 }));
 
 module.exports = router;
