a12a7721b2f4fe3bcc9fdcf9b9fc849795571a9d
Huan Ai <aihuan96@gmail.com>
2021-08-26 18:45:06 -0400

    added delete game function

--- public/javascripts/add-game.js
+++ public/javascripts/add-game.js
@@ -1,9 +1,4 @@
 
-// const reviewDelete = document.querySelector(".reviewDelete")
-// reviewDelete.addEventListener("click", async (event) => {
-//     await fetch(`/api/reviews/${event.target.id}`, {method: 'DELETE'});
-//     event.target.parentElement.parentElement.remove()
-// })
 
 
 const allButtons = document.querySelectorAll('.addGameBtn');
---
+++ public/javascripts/delete-game.js
@@ -0,0 +1,26 @@
+
+// const allButtons = document.querySelectorAll('.addGameBtn');
+// for (let button of allButtons) {
+//     button.addEventListener('click', async (event) => {
+//         const gameId = event.target.parentElement.parentElement.id;
+//         const selectCurrentVal = document.querySelector(`.select-${gameId}`);
+//         const collectionId = selectCurrentVal.options[selectCurrentVal.selectedIndex].value;
+//         const value = await fetch(`/api/collections/${collectionId}`, { method: 'POST', body: JSON.stringify({ gameId }), headers: { 'Content-Type': 'application/json' } })
+//         alert("Game has been added!")
+//         console.log(value.body);
+//     })
+// }
+
+const allButtons = document.querySelectorAll('.removeGame');
+if (allButtons) {
+    for (let button of allButtons) {
+        button.addEventListener('click', async (event) => {
+            const gameId = event.target.parentElement.parentElement.id;
+            const collectionId = document.querySelector(".collectionName").id;
+            const value = await fetch(`/api/collections/${collectionId}/games`, { method: 'DELETE', body: JSON.stringify({ gameId }), headers: { 'Content-Type': 'application/json' } });
+            const row = document.getElementById(gameId);
+            row.remove();
+            alert("Game has been deleted!");
+        })
+    }
+}
